70f9ccff04119e64e7ff24dde47fde2a6e81784d
PapaKarlos97 <97maksimov97@gmail.com>
2023-05-19 15:06:36 +0300

    addFunctional to Actor

--- src/main/java/io/vladimir/easyApp/workingArea/models/Actor.java
+++ src/main/java/io/vladimir/easyApp/workingArea/models/Actor.java
@@ -1,14 +1,64 @@
 package io.vladimir.easyApp.workingArea.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import javax.lang.model.element.Name;
+import java.util.List;
 
 @Entity
 public class Actor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_actor;
+    @Column(name = "name_actor")
+    private String name;
+    @Column(name = "age")
+    private int age;
+    @ManyToMany
+    @JoinTable(
+            name = "compatible",
+            joinColumns = @JoinColumn (name = "id_actor"),
+            inverseJoinColumns = @JoinColumn (name = "id_films")
+    )
+    List<Movie>movies;
 
+    public Actor() {
+    }
+
+    public Actor(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId_actor() {
+        return id_actor;
+    }
+
+    public void setId_actor(int id_actor) {
+        this.id_actor = id_actor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
 }
