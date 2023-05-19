261db4a702f5801ce9139d630fb45bb3e4cfc5b8
PapaKarlos97 <97maksimov97@gmail.com>
2023-05-19 21:02:06 +0300

    add functional to Services

--- src/main/java/io/vladimir/easyApp/workingArea/service/ServiceForPerson.java
+++ src/main/java/io/vladimir/easyApp/workingArea/service/ServiceForPerson.java
@@ -1,4 +1,27 @@
 package io.vladimir.easyApp.workingArea.service;
 
+import io.vladimir.easyApp.workingArea.models.Person;
+import io.vladimir.easyApp.workingArea.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ServiceForPerson {
+    private final PersonRepository personRepository;
+    @Autowired
+    public ServiceForPerson(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public List<Person> findAll(){
+        return personRepository.findAll();
+    }
+    public Person findById(int id){
+         Optional<Person> findPerson =personRepository.findById(id);
+    return findPerson.orElse(null);
+    }
+
 }
